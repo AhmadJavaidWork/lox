bd42661d0f0feedb05ed6a2005dfd0800599eecf
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-15 15:11:33 +0500

    added environment and variable initialization

--- src/Stmt.java
+++ src/Stmt.java
@@ -1,15 +1,12 @@
-abstract class Stmt {
-    abstract <R> R accept(Visitor<R> visitor);
+import java.util.List;
 
+abstract class Stmt {
     interface Visitor<R> {
         R visitExpressionStmt(Expression stmt);
-
         R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
     }
-
     static class Expression extends Stmt {
-        final Expr expression;
-
         Expression(Expr expression) {
             this.expression = expression;
         }
@@ -18,11 +15,11 @@ abstract class Stmt {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitExpressionStmt(this);
         }
+
+        final Expr expression;
     }
 
     static class Print extends Stmt {
-        final Expr expression;
-
         Print(Expr expression) {
             this.expression = expression;
         }
@@ -31,5 +28,25 @@ abstract class Stmt {
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitPrintStmt(this);
         }
+
+        final Expr expression;
     }
+
+    static class Var extends Stmt {
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
+
+        final Token name;
+        final Expr initializer;
+    }
+
+
+    abstract <R> R accept(Visitor<R> visitor);
 }
