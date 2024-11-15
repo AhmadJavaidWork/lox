abee58cbae77732bac3b5801182589cb53158a38
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-15 13:41:21 +0500

    added statements and print statement

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,8 +1,11 @@
-public class Interpreter implements Expr.Visitor<Object> {
-    void interpret(Expr expression) {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
@@ -37,7 +40,7 @@ public class Interpreter implements Expr.Visitor<Object> {
             case TokenType.MINUS:
                 checkNumberOperand(expr.operator, right);
                 return -(double) right;
-        };
+        }
         return null;
 
     }
@@ -67,6 +70,23 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 
     @Override
